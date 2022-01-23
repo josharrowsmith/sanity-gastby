ec691c5c6ea7662d1e745a0760812352eccf0104
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2022-01-24 06:52:21 +1000

    link

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { getSanityImageUrl } from "../lib/image-url";
 
-const imageGrid = ({ columns }) => {
+const stores = ({ columns }) => {
   return (
     <section className="container mx-auto pt-10 ss:px-4 grid lg:grid-cols-3 grid-rows-1 ss:grid-cols-2 justify-between gap-3">
       {columns.map(i => {
         return (
-          <div className="stores">
-            <img src={getSanityImageUrl(i.asset.url, 600)} style={{ height: "100%" }} />
-          </div>
+          <a href={i.cta ? i.cta.link : null} target="_blank" className="stores">
+            <img src={getSanityImageUrl(i.image.asset.url, 600)} style={{ height: "100%" }} />
+          </a>
         );
       })}
     </section>
   );
 };
 
-export default imageGrid;
+export default stores;
