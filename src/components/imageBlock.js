445c1e48cbfbdecdb151616816c199f503b86490
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2022-01-24 20:44:26 +1000

    okay

--- src/components/imageBlock.js
+++ src/components/imageBlock.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { getSanityImageUrl } from "../lib/image-url";
 import classnames from "classnames";
-import useWindowSize from "../hooks/useGatsbyWindowSize"
+import useWindowSize from "../hooks/useGatsbyWindowSize";
 
 const imageBlock = ({ columns, styles, _type, size }) => {
-  const { width } = useWindowSize()
+  const { width } = useWindowSize();
 
   return (
     <section className={classnames("container mx-auto pt-10 ss:px-4 grid ", styles)}>
       {columns.map(i => {
         if (i.disabled && width < 767) return;
         return (
-          <a key={_type.id} a href={i.cta ? i.cta.link : undefined} target="_blank" className={_type}>
-            <img src={getSanityImageUrl(i.image.asset.url, size)} className={classnames("h-full")} alt="stuff" />
+          <a
+            key={_type.id}
+            a
+            href={i.cta ? i.cta.link : undefined}
+            target="_blank"
+            className={_type}
+          >
+            <img
+              src={getSanityImageUrl(i.image.asset.url, size)}
+              className={classnames("block")}
+              alt="stuff"
+            />
           </a>
         );
       })}
