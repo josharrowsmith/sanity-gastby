a755224d22f85dd8a07c9abca2d57d52e2dce813
Josh Arrowsmith <josh.arrowsmith@hotmail.com>
2022-01-25 12:42:27 +1000

    ios

--- src/components/imageBlock.js
+++ src/components/imageBlock.js
@@ -19,7 +19,7 @@ const imageBlock = ({ columns, styles, _type, size }) => {
           >
             <img
               src={getSanityImageUrl(i.image.asset.url, size)}
-              className={classnames("min-h-full h-auto")}
+              className={classnames("w-full")}
               alt="stuff"
             />
           </a>
